67016075198f34a7cdb7816e54b931790a16df21
0Mollis <maks.chudnov.05@mail.ru>
2025-02-02 13:04:44 +0300

    Сделал каталог прокручиваемым

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let scrollAmount = 0;
+let catalog = document.querySelector('.catalog');
+
+function scrollCatalog(direction) {
+    let itemWidth = catalog.querySelector('.catalog-item').offsetWidth + 32; // 32 - это margin
+    scrollAmount += direction * itemWidth;
+    // Защита от прокрутки за границы
+    let maxScroll = catalog.scrollWidth - catalog.parentNode.clientWidth;
+    if (scrollAmount < 0) {
+        scrollAmount = 0;
+    } else if (scrollAmount > maxScroll) {
+        scrollAmount = maxScroll;
+    }
+    // Устанавливаем новое положение каталога
+    catalog.style.transform = `translateX(-${scrollAmount}px)`;
+
+    
+}
