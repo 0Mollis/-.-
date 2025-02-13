f1548de395209fded81edc3c1722b8cba9fed608
0Mollis <maks.chudnov.05@mail.ru>
2025-02-13 21:53:09 +0300

    4.5/5 всё кое-где до делать

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,10 +3,11 @@
 
 // Write your JavaScript code.
 
-let scrollAmount = 0;
-let catalog = document.querySelector('.catalog');
 
 function scrollCatalog(direction) {
+    let scrollAmount = 0;
+    let catalog = document.querySelector('.catalog');
+
     let itemWidth = catalog.querySelector('.catalog-item').offsetWidth + 35.2; // 35.2 - это margin
     scrollAmount += direction * itemWidth;
     // Защита от прокрутки за границы
@@ -18,6 +19,43 @@ function scrollCatalog(direction) {
     }
     // Устанавливаем новое положение каталога
     catalog.style.transform = `translateX(-${scrollAmount}px)`;
+}
+check_quantity();
+function quantity_plus() {
+    var quantity = document.querySelector('.quantity-col');
+    var maxquantity = document.querySelector('.maxquantity');
+    quantity.textContent = Number(quantity.textContent) + 1;
+    check_quantity();
+}
 
-    
+function check_quantity(){
+    var quantity = document.querySelector('.quantity-col');
+    var btn = document.querySelector('.button-quantity')
+    var mbtn = document.querySelector('.mbutton-quantity')
+    var maxquantity = document.querySelector('.maxquantity');
+    if (Number(quantity.textContent) + 1 > maxquantity.textContent) {
+        btn.disabled = true;
+        btn.classList.remove('quantity-plus');
+        btn.classList.add('quantity-min');
+    }
+    else{
+        btn.disabled = false;
+        btn.classList.add('quantity-plus')
+    }
+    if (Number(quantity.textContent) - 1 < 1) {
+        mbtn.disabled = true;
+        mbtn.classList.remove('quantity-plus');
+        mbtn.classList.add('quantity-min');
+    }
+    else {
+        mbtn.disabled = false;
+        mbtn.classList.add('quantity-plus')
+    }
+}
+
+function quantity_minus() {
+    var quantity = document.querySelector('.quantity-col');
+    var maxquantity = document.querySelector('.maxquantity');
+    quantity.textContent = Number(quantity.textContent) - 1;
+    check_quantity();
 }
